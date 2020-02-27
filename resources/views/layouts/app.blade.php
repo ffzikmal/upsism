14db95979dd3a548bcbaa4ae589fc293b1792f46
ikmal ffz <ffzikmal12@gmail.com>
2020-02-27 10:52:05 +0800

    second

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,9 +49,6 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item pr-3">
-                                <a class="nav-link" href="/profile/{{ $post->user->id }}">Profile</a>
-                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->username }} <span class="caret"></span>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,9 +13,6 @@
                         <div>
                             <h5 class="font-weight-bold">{{ $post->user->username }}</h5>
                         </div>
-                        <div>
-                            <button onclick="handleDelete({{ $post->id }})" class="btn btn-danger">Delete</button>
-                        </div>
                     </div>
                 </div>
 
@@ -23,30 +20,6 @@
 
                 <div>{{ $post->caption }}</div>
             </div>
-        </div>
-    </div>
-
-    <div class="modal" id="modalDelete" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <form id="formDelete" action="" method="post">
-                @csrf
-                @method('delete')
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Warning !</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Are you sure want to delete?</p>
-                    </div>
-                    <div class="modal-footer">
-                        <input type="submit" class="btn btn-danger" value="Delete">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    </div>
-                </div>
-            </form>
         </div>
     </div>
 
